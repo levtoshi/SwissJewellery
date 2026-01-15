e4d2fcfe33a1999cf06c4d261c4540dcfc9f3191
levtoshi <Intel@SATOSHI-LAPTOP>
2026-01-15 13:21:38 +0200

    Fix console errors & write refreshToken to DB

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useState } from 'react';
 import { authAPI } from '../api/auth';
 import { useQuery, useQueryClient } from '@tanstack/react-query';
 
@@ -7,27 +7,35 @@ const AuthContext = createContext(null);
 export const AuthProvider = ({ children }) => {
   const queryClient = useQueryClient();
 
-  const { data: user, isLoading } = useQuery({
+  const [token, setToken] = useState(
+    () => localStorage.getItem('accessToken')
+  );
+
+  const { data: user, isLoading, error } = useQuery({
     queryKey: ['me'],
-    queryFn: async () => await authAPI.getMe()
+    queryFn: async () => await authAPI.getMe(),
+    enabled: !!token
   });
 
   const login = async (credentials) => {
     const data = await authAPI.login(credentials);
     localStorage.setItem('accessToken', data.accessToken);
     queryClient.refetchQueries({ queryKey: ['me'] });
+    setToken(data.accessToken);
   };
 
   const register = async (userData) => {
     const data = await authAPI.register(userData);
     localStorage.setItem('accessToken', data.accessToken);
     queryClient.refetchQueries({ queryKey: ['me'] });
+    setToken(data.accessToken);
   };
 
   const logout = async () => {
     await authAPI.logout();
     localStorage.removeItem('accessToken');
     queryClient.setQueryData(['me'], null);
+    setToken(null);
   };
 
   return (
